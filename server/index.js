412e9e35cba5106414484498c4a0c04a8cc0f842
DianaSmertina <am.dianasmertina@gmail.com>
2023-09-04 17:08:28 +0300

    feat: add orm with users table

--- server/index.js
+++ server/index.js
@@ -1,16 +1,31 @@
 const express = require("express");
 const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+const sequelize = require("./db");
+const models = require("./models/models");
+require("pg");
 
 class Server {
     app = express();
-    start() {
+    async start() {
         try {
+            await sequelize.authenticate();
+            await sequelize.sync();
             const currentServer = this.app.listen(PORT, () => {
                 console.log(`server start on port ${PORT}`);
             });
         } catch (e) {
             console.log(e);
         }
+    }
+
+    addMiddleware() {
+        this.app.use(
+            cors({
+                origin: ["https://sweet-kheer-626c63.netlify.app", "http://localhost:5173/"],
+            })
+        );
+        this.app.use(express.json());
     }
 }
 
