0cb17bc4419a9ca07018f4cd9a62216bf9eee72c
DianaSmertina <am.dianasmertina@gmail.com>
2023-09-08 20:49:06 +0300

    feat: add navigation in header

--- client/src/components/layout/header/Header.tsx
+++ client/src/components/layout/header/Header.tsx
@@ -1,14 +1,26 @@
-import { Row, Navbar, Container, Nav } from "react-bootstrap";
+import { Row, Navbar, Container, Nav, Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 function Header() {
     return (
         <Row className="w-100 p-0 m-0">
             <header className="p-0">
-                <Navbar bg="light" data-bs-theme="light" expand="lg">
+                <Navbar data-bs-theme="light" className="bg-body-tertiary justify-content-between">
                     <Container>
-                        <Navbar.Brand href="/">Recommend!</Navbar.Brand>
+                        <Navbar.Brand><NavLink to="/" className="text-decoration-none link-dark">Recommend!</NavLink></Navbar.Brand>
                         <Nav>
-
+                            {localStorage.getItem("token") ? (
+                                <>
+                                    <Nav.Link>userimg</Nav.Link>
+                                    <Button>Log out</Button>
+                                </>
+                            ) : (
+                                <>
+                                    <NavLink to="/sign-in"><Button>Sign in</Button></NavLink>
+                                    <NavLink to="/sign-up"><Button>Sign up</Button></NavLink>
+                                </>
+                            )}
+                            
                         </Nav>
                     </Container>
                 </Navbar>
