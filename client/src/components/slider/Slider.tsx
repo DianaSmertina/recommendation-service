c95246cc14b990e94f6756a3b99a75a84e1d27e9
DianaSmertina <am.dianasmertina@gmail.com>
2023-09-10 12:54:33 +0300

    feat: add styles for scroll panels

--- client/src/components/slider/Slider.tsx
+++ client/src/components/slider/Slider.tsx
@@ -4,23 +4,49 @@ import { IReviewsResponse } from "../../types/types";
 import ReviewCard from "../reviewCard/ReviewCard";
 
 import styles from "./slider.module.scss";
+import { useRef } from "react";
 
 interface ISliderProps {
     reviews: Array<IReviewsResponse>;
 }
 
 function Slider({ reviews }: ISliderProps) {
+    const slider = useRef<HTMLDivElement>(null);
+
+    const arrowClickHandler = (isLeft: boolean) => {
+        if (slider.current) {
+            isLeft
+                ? (slider.current.scrollLeft += 200)
+                : (slider.current.scrollLeft -= 200);
+        }
+    };
+
     return (
-        <Container className="d-flex align-items-center justify-content-between p-0">
-            <Image src="./arrow-left.png" width={50} height={50} />
-            <div className={`d-flex m-auto ${styles.slider}`}>
-                {reviews.length > 0 ? (
-                    reviews.map((el) => <ReviewCard key={el.id} review={el} />)
-                ) : (
-                    <Spinner />
-                )}
-            </div>
-            <Image src="./arrow-right.png" width={50} height={50} />
+        <Container className="d-flex align-items-center justify-content-between p-0 position-relative">
+            <Image
+                src="./arrow-left.png"
+                width={50}
+                height={50}
+                onClick={() => arrowClickHandler(false)}
+            />
+            {reviews.length > 0 ? (
+                <div
+                    className={`d-flex m-auto position-relative slider ${styles.slider}`}
+                    ref={slider}
+                >
+                    {reviews.map((el) => (
+                        <ReviewCard key={el.id} review={el} />
+                    ))}
+                </div>
+            ) : (
+                <Spinner />
+            )}
+            <Image
+                src="./arrow-right.png"
+                width={50}
+                height={50}
+                onClick={() => arrowClickHandler(true)}
+            />
         </Container>
     );
 }
