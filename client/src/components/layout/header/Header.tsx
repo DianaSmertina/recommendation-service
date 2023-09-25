be515c8bb7e10d83c8557e83a3857fcc239e77aa
DianaSmertina <am.dianasmertina@gmail.com>
2023-09-25 12:43:38 +0800

    feat: add burger in header

--- client/src/components/layout/header/Header.tsx
+++ client/src/components/layout/header/Header.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Row, Navbar, Container, Nav, Button } from "react-bootstrap";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
@@ -18,6 +18,7 @@ function Header() {
     const { t, i18n } = useTranslation();
     const currentLang = localStorage.getItem("lang");
     const [language, setLanguage] = useState(currentLang || "ENG");
+    const [showNav, setShowNav] = useState(false);
 
     const logOut = async () => {
         await UserApi.logout();
@@ -34,54 +35,65 @@ function Header() {
         i18n.changeLanguage(toggledLanguage);
     };
 
+    const toggleNav = () => {
+        setShowNav(!showNav);
+    };
+
     return (
-        <Row className="w-100 p-0 m-0">
-            <header className="p-0">
-                <Navbar className="justify-content-between">
-                    <Container>
-                        <Navbar.Brand>
-                            <NavLink
-                                to="/"
-                                className="text-decoration-none link-dark"
-                            >
-                                <img
-                                    src={logo}
-                                    alt="review logo"
-                                />
-                            </NavLink>
-                        </Navbar.Brand>
-                        <Nav>
-                            <Button
-                                variant="outline-primary"
-                                className="mx-2 me-1"
-                                onClick={clickLangHandler}
-                            >
-                                {language}
-                            </Button>
-                            {currentUserId ? (
-                                <>
-                                    <NavLink to={`/user/${currentUserId}`}>
-                                        <Button className={`${styles.btn} me-1`}>{t("my-page")}</Button>
-                                    </NavLink>
-                                    <Button onClick={logOut} className={styles.btn}>
-                                        {t("log-out")}
+        <Navbar expand="md" className="mb-4 w-100">
+            <Container>
+                <Navbar.Brand
+                    as={NavLink}
+                    to="/"
+                    className="me-md-auto text-decoration-none link-dark"
+                >
+                    <img src={logo} alt="review logo" />
+                </Navbar.Brand>
+                <Navbar.Toggle
+                    aria-controls="responsive-navbar-nav"
+                    onClick={toggleNav}
+                />
+                <Navbar.Collapse
+                    id="responsive-navbar-nav"
+                    className={showNav ? "show" : ""}
+                >
+                    <Nav className="ms-auto">
+                        <Button
+                            variant="outline-primary"
+                            className={`${styles.btn} my-2 ms-auto`}
+                            onClick={clickLangHandler}
+                        >
+                            {language}
+                        </Button>
+                        {currentUserId ? (
+                            <>
+                                <Nav.Link as={NavLink} to={`/user/${currentUserId}`} className="ms-auto">
+                                    <Button className={`${styles.btn}`}>
+                                        {t("my-page")}
                                     </Button>
-                                </>
-                            ) : (
-                                <>
-                                    <NavLink to="/sign-in" >
-                                        <Button className={`${styles.btn} me-1`}>{t("sign-in")}</Button>
-                                    </NavLink>
-                                    <NavLink to="/sign-up">
-                                        <Button className={styles.btn}>{t("sign-up")}</Button>
-                                    </NavLink>
-                                </>
-                            )}
-                        </Nav>
-                    </Container>
-                </Navbar>
-            </header>
-        </Row>
+                                </Nav.Link>
+                                <Button onClick={logOut} className={`${styles.btn } ms-auto my-2`}>
+                                    {t("log-out")}
+                                </Button>
+                            </>
+                        ) : (
+                            <>
+                                <Nav.Link as={NavLink} to="/sign-in" className="ms-auto pe-0">
+                                    <Button className={`${styles.btn}`}>
+                                        {t("sign-in")}
+                                    </Button>
+                                </Nav.Link>
+                                <Nav.Link as={NavLink} to="/sign-up" className="ms-auto">
+                                    <Button className={styles.btn}>
+                                        {t("sign-up")}
+                                    </Button>
+                                </Nav.Link>
+                            </>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
